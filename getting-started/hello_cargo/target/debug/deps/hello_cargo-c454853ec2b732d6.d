a3c84bcb8dd2a6bbbbb950fff5a8f57712f5a1e4
Abdullahi Hussein <abdullahi.a.hussein@gmail.com>
2025-10-08 16:23:29 -0400

    starting steps for rust: running first rust code, using cargo for the first time

---
+++ getting-started/hello_cargo/target/debug/deps/hello_cargo-c454853ec2b732d6.d
@@ -0,0 +1,5 @@
+/Users/abdul/dev/projects/languages/rust-dip/getting-started/hello_cargo/target/debug/deps/hello_cargo-c454853ec2b732d6.d: src/main.rs
+
+/Users/abdul/dev/projects/languages/rust-dip/getting-started/hello_cargo/target/debug/deps/hello_cargo-c454853ec2b732d6: src/main.rs
+
+src/main.rs:
---
+++ getting-started/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/abdul/dev/projects/languages/rust-dip/getting-started/hello_cargo/target/debug/hello_cargo: /Users/abdul/dev/projects/languages/rust-dip/getting-started/hello_cargo/src/main.rs
