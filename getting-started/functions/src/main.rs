2ad8fbba0b2563a3cce64633b5cae8ac95cf92f8
Abdullahi Hussein <abdullahi.a.hussein@gmail.com>
2025-10-12 22:53:48 -0400

    functions review complete

--- getting-started/functions/src/main.rs
+++ getting-started/functions/src/main.rs
@@ -2,6 +2,13 @@ fn main() {
     println!("Hello, world!");
     another_function();
     second_function(10);
+    looper();
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
+     
 }
 fn another_function() {
     println!("Another function");
@@ -9,4 +16,19 @@ fn another_function() {
 
 fn second_function(x: i32) {
     println!("The value of x is: {x}");
+}
+
+fn looper() {
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+}
+
+fn five() -> i32 {
+    5
 }
